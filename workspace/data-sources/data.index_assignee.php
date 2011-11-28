6742f3e7b80e00f1e0d6251225eb676f42085438
Nils Werner <nils.werner@gmail.com>
2011-11-28 11:43:49 +0100

    Index Assignee DS points to Members Section

--- workspace/data-sources/data.index_assignee.php
+++ workspace/data-sources/data.index_assignee.php
@@ -1,55 +1,61 @@
 <?php
 
 	require_once(TOOLKIT . '/class.datasource.php');
-	
+
 	Class datasourceindex_assignee extends Datasource{
-		
+
 		public $dsParamROOTELEMENT = 'index-assignee';
 		public $dsParamORDER = 'desc';
+		public $dsParamPAGINATERESULTS = 'yes';
 		public $dsParamLIMIT = '999';
-		public $dsParamREDIRECTONEMPTY = 'no';
-		public $dsParamSORT = 'none';
 		public $dsParamSTARTPAGE = '1';
+		public $dsParamREDIRECTONEMPTY = 'no';
+		public $dsParamSORT = 'system:id';
 		public $dsParamASSOCIATEDENTRYCOUNTS = 'yes';
+
+		
+
 		public $dsParamINCLUDEDELEMENTS = array(
-				'username',
-				'role',
 				'name',
-				'company',
+				'username',
 				'email',
-				'none',
+				'role',
+				'role: permissions',
+				'company',
 				'detailed',
 				'hide',
-				'suscriptions'
+				'subscriptions'
 		);
+
 
 		public function __construct(&$parent, $env=NULL, $process_params=true){
 			parent::__construct($parent, $env, $process_params);
 			$this->_dependencies = array();
 		}
-		
+
 		public function about(){
 			return array(
-					 'name' => 'Index Assignee',
-					 'author' => array(
-							'name' => 'Nils Werner',
-							'website' => 'http://trac.obsessive-media.de.localhost',
-							'email' => 'nils.werner@gmail.com'),
-					 'version' => '1.0',
-					 'release-date' => '2010-02-16T14:49:41+00:00');	
+				'name' => 'Index Assignee',
+				'author' => array(
+					'name' => 'Default Administrator',
+					'website' => 'http://bugaroo.localhost',
+					'email' => 'nobody@bugaroo.symphony-cms.com'),
+				'version' => 'Symphony 2.2.2',
+				'release-date' => '2011-11-28T10:42:55+00:00'
+			);
 		}
-		
+
 		public function getSource(){
-			return '7';
+			return '15';
 		}
-		
+
 		public function allowEditorToParse(){
 			return true;
 		}
-		
+
 		public function grab(&$param_pool=NULL){
 			$result = new XMLElement($this->dsParamROOTELEMENT);
-				
+
 			try{
 				include(TOOLKIT . '/data-sources/datasource.section.php');
 			}
@@ -57,14 +63,17 @@
 				// Work around. This ensures the 404 page is displayed and
 				// is not picked up by the default catch() statement below
 				FrontendPageNotFoundExceptionHandler::render($e);
-			}			
+			}
 			catch(Exception $e){
 				$result->appendChild(new XMLElement('error', $e->getMessage()));
 				return $result;
-			}	
+			}
 
 			if($this->_force_empty_result) $result = $this->emptyXMLSet();
+
+			
+
 			return $result;
 		}
-	}
 
+	}
