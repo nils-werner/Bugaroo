93d976b4da71e9a2c536695342af89a5ecebbbb4
Nils Werner <nils.werner@gmail.com>
2011-11-28 11:36:47 +0100

    Show only assigned projects on Dashboard and Project View

--- workspace/data-sources/data.dashboard_projects.php
+++ workspace/data-sources/data.dashboard_projects.php
@@ -1,22 +1,24 @@
 <?php
 
 	require_once(TOOLKIT . '/class.datasource.php');
-	
+
 	Class datasourcedashboard_projects extends Datasource{
-		
+
 		public $dsParamROOTELEMENT = 'dashboard-projects';
 		public $dsParamORDER = 'asc';
+		public $dsParamPAGINATERESULTS = 'yes';
 		public $dsParamLIMIT = '20';
+		public $dsParamSTARTPAGE = '1';
 		public $dsParamREDIRECTONEMPTY = 'no';
 		public $dsParamPARAMOUTPUT = 'system:id';
 		public $dsParamSORT = 'order';
-		public $dsParamSTARTPAGE = '1';
 		public $dsParamASSOCIATEDENTRYCOUNTS = 'no';
-		
+
 		public $dsParamFILTERS = array(
 				'28' => '{$url-status:Active}',
+				'29' => '{$member-id}',
 		);
-		
+
 		public $dsParamINCLUDEDELEMENTS = array(
 				'title',
 				'description: formatted',
@@ -25,33 +27,35 @@
 				'administrators'
 		);
 
+
 		public function __construct(&$parent, $env=NULL, $process_params=true){
 			parent::__construct($parent, $env, $process_params);
 			$this->_dependencies = array();
 		}
-		
+
 		public function about(){
 			return array(
-					 'name' => 'Dashboard Projects',
-					 'author' => array(
-							'name' => 'Nils Werner',
-							'website' => 'http://trac.obsessive-media.de.localhost',
-							'email' => 'nils.werner@gmail.com'),
-					 'version' => '1.0',
-					 'release-date' => '2010-02-11T14:08:58+00:00');	
+				'name' => 'Dashboard Projects',
+				'author' => array(
+					'name' => 'Default Administrator',
+					'website' => 'http://bugaroo.localhost',
+					'email' => 'nobody@bugaroo.symphony-cms.com'),
+				'version' => 'Symphony 2.2.2',
+				'release-date' => '2011-11-28T10:34:43+00:00'
+			);
 		}
-		
+
 		public function getSource(){
 			return '8';
 		}
-		
+
 		public function allowEditorToParse(){
 			return true;
 		}
-		
+
 		public function grab(&$param_pool=NULL){
 			$result = new XMLElement($this->dsParamROOTELEMENT);
-				
+
 			try{
 				include(TOOLKIT . '/data-sources/datasource.section.php');
 			}
@@ -59,14 +63,17 @@
 				// Work around. This ensures the 404 page is displayed and
 				// is not picked up by the default catch() statement below
 				FrontendPageNotFoundExceptionHandler::render($e);
-			}			
+			}
 			catch(Exception $e){
 				$result->appendChild(new XMLElement('error', $e->getMessage()));
 				return $result;
-			}	
+			}
 
 			if($this->_force_empty_result) $result = $this->emptyXMLSet();
+
+			
+
 			return $result;
 		}
-	}
 
+	}
--- workspace/data-sources/data.project_project.php
+++ workspace/data-sources/data.project_project.php
@@ -1,23 +1,25 @@
 <?php
 
 	require_once(TOOLKIT . '/class.datasource.php');
-	
+
 	Class datasourceproject_project extends Datasource{
-		
+
 		public $dsParamROOTELEMENT = 'project-project';
 		public $dsParamORDER = 'desc';
+		public $dsParamPAGINATERESULTS = 'yes';
 		public $dsParamLIMIT = '1';
+		public $dsParamSTARTPAGE = '1';
 		public $dsParamREDIRECTONEMPTY = 'yes';
 		public $dsParamREQUIREDPARAM = '$project';
 		public $dsParamPARAMOUTPUT = 'system:id';
 		public $dsParamSORT = 'system:id';
-		public $dsParamSTARTPAGE = '1';
 		public $dsParamASSOCIATEDENTRYCOUNTS = 'yes';
-		
+
 		public $dsParamFILTERS = array(
 				'26' => '{$project}',
+				'29' => '{$member-id}',
 		);
-		
+
 		public $dsParamINCLUDEDELEMENTS = array(
 				'title',
 				'description: formatted',
@@ -26,33 +28,35 @@
 				'administrators'
 		);
 
+
 		public function __construct(&$parent, $env=NULL, $process_params=true){
 			parent::__construct($parent, $env, $process_params);
 			$this->_dependencies = array();
 		}
-		
+
 		public function about(){
 			return array(
-					 'name' => 'Project Project',
-					 'author' => array(
-							'name' => 'Nils Werner',
-							'website' => 'http://trac.obsessive-media.de.localhost',
-							'email' => 'nils.werner@gmail.com'),
-					 'version' => '1.0',
-					 'release-date' => '2010-02-08T00:16:15+00:00');	
+				'name' => 'Project Project',
+				'author' => array(
+					'name' => 'Default Administrator',
+					'website' => 'http://bugaroo.localhost',
+					'email' => 'nobody@bugaroo.symphony-cms.com'),
+				'version' => 'Symphony 2.2.2',
+				'release-date' => '2011-11-28T10:35:48+00:00'
+			);
 		}
-		
+
 		public function getSource(){
 			return '8';
 		}
-		
+
 		public function allowEditorToParse(){
 			return true;
 		}
-		
+
 		public function grab(&$param_pool=NULL){
 			$result = new XMLElement($this->dsParamROOTELEMENT);
-				
+
 			try{
 				include(TOOLKIT . '/data-sources/datasource.section.php');
 			}
@@ -60,14 +64,17 @@
 				// Work around. This ensures the 404 page is displayed and
 				// is not picked up by the default catch() statement below
 				FrontendPageNotFoundExceptionHandler::render($e);
-			}			
+			}
 			catch(Exception $e){
 				$result->appendChild(new XMLElement('error', $e->getMessage()));
 				return $result;
-			}	
+			}
 
 			if($this->_force_empty_result) $result = $this->emptyXMLSet();
+
+			
+
 			return $result;
 		}
-	}
 
+	}
